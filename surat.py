8ecbd423f4abdbbd6ee9dbe17c725d8fa2c18314
Ahmet Levent Tasel <leventtasel@gmail.com>
2020-03-27 10:26:21 -0700

    tuning

--- surat.py
+++ surat.py
@@ -228,7 +228,7 @@ class Model(nn.Module):
 
 
 def train():
-    batchSize = 1500
+    batchSize = 16
     dataSet = Data()
     dataLoader = DataLoader(
         dataset=dataSet,
@@ -240,7 +240,7 @@ def train():
     model = Model(dataSet.count, filterMood=False).to(DEVICE)
     modelOptimizer = torch.optim.Adam(
         model.parameters(),
-        lr=1e-2
+        lr=1e-3
     )
 
     epochCount = 50000
